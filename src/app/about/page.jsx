7c6bff719604acde1f043cd82c58da5be676f28b
Sergii Burgazlieiev <burgazlieiev@gmail.com>
2024-06-28 10:55:15 -0500

    about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -4,13 +4,34 @@ import styles from './about.module.css';
 
 const AboutPage = () => {
 	return (
-		<div>
-			<div className={styles.imageContainer}>
-				<Image
-					src='https://images.pexels.com/photos/26652842/pexels-photo-26652842/free-photo-of-a-stairway-leading-up-to-a-glass-building.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
-					alt=''
-					fill
-				/>
+		<div className={styles.container}>
+			<div className={styles.textContainer}>
+				<h2 className={styles.subTitle}>About Agency</h2>
+				<h1 className={styles.title}>
+					We create captivating visuals & compelling content.
+				</h1>
+				<p className={styles.desc}>
+					Innovate Agency specializes in crafting unique and impactful brand
+					experiences through cutting-edge design, innovative strategies, and
+					creative storytelling.
+				</p>
+				<div className={styles.boxes}>
+					<div className={styles.box}>
+						<h1>10 K+</h1>
+						<p>Year of experience</p>
+					</div>
+					<div className={styles.box}>
+						<h1>234 K+</h1>
+						<p>People reached</p>
+					</div>
+					<div className={styles.box}>
+						<h1>5K+</h1>
+						<p>Services and plugins</p>
+					</div>
+				</div>
+			</div>
+			<div className={styles.imgContainer}>
+				<Image className={styles.img} src='/about.png' alt='About image' fill />
 			</div>
 		</div>
 	);
