bec1d7965305fe8a72d3bd0f3b0f1cf2541c50f5
Sergii Burgazlieiev <burgazlieiev@gmail.com>
2024-06-28 11:23:47 -0500

    contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import Image from 'next/image';
+import styles from './contact.module.css';
 
 const ContactPage = () => {
-	return <div>Contact Page</div>;
+	return (
+		<div className={styles.container}>
+			<div className={styles.imgContainer}>
+				<Image
+					className={styles.img}
+					src='/contact.png'
+					alt='Contact image'
+					fill
+				/>
+			</div>
+			<div className={styles.formContainer}>
+				<form action='' className={styles.form}>
+					<input type='text' placeholder='Name and Surname' name='name' id='' />
+					<input type='email' placeholder='Email Address' name='email' id='' />
+					<input type='phone' placeholder='Phone Number' name='phone' id='' />
+					<textarea
+						name='message'
+						id=''
+						cols='30'
+						rows='10'
+						placeholder='Message'
+					/>
+					<button>Send</button>
+				</form>
+			</div>
+		</div>
+	);
 };
 
 export default ContactPage;
