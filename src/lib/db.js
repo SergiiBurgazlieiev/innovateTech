d708b015bd900206e0c5c3918b835452b575cf17
Sergii Burgazlieiev <burgazlieiev@gmail.com>
2024-07-01 17:20:30 -0500

    mongoose User and Post models

---
+++ src/lib/db.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+const connection = {};
+
+export const connectToDB = async () => {
+	try {
+		if (connection.isConnected) {
+			console.log('Using existing connection');
+			return;
+		}
+		const db = await mongoose.connect(process.env.MONGO);
+		connection.isConnected = db.connection[0].readyState;
+	} catch (err) {
+		console.log(err);
+		throw new Error(err);
+	}
+};
