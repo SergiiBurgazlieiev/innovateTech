2e2595fe1806d11c7160d31ef3a6fb5c3a5653e5
Sergii Burgazlieiev <burgazlieiev@gmail.com>
2024-07-07 16:18:23 -0500

    admin user form refactor

--- src/components/adminUserForm/AdminUserForm.jsx
+++ src/components/adminUserForm/AdminUserForm.jsx
@@ -1,29 +1,36 @@
 'use client';
 import { useFormState } from 'react-dom';
 import { addUser } from '@/lib/actions';
+import { Form } from '../formElements/form/Form';
+import { Input } from '../formElements/input/Input';
+import { Button } from '../formElements/button/Button';
 import styles from './AdminUserForm.module.css';
+
+const options = [
+	{ value: false, label: 'Is Admin?' },
+	{ value: false, label: 'No' },
+	{ value: true, label: 'Yes' },
+];
 
 export const AdminUserForm = () => {
 	const [state, formAction] = useFormState(addUser, undefined);
 
 	return (
-		<form action={formAction} className={styles.container}>
-			<h1>Add New User</h1>
-			<input type='text' name='username' placeholder='Enter your username' />
-			<input type='email' name='email' placeholder='Enter your email' />
-			<input
-				type='password'
-				name='password'
-				placeholder='Enter your password'
-			/>
-			<input type='text' name='avatar' placeholder='Add your avatar image' />
-			<select name='isAdmin'>
-				<option value={false}>Is Admin?</option>
-				<option value={false}>No</option>
-				<option value={true}>Yes</option>
-			</select>
-			<button>Add User</button>
-			{state?.error}
-		</form>
+		<div className={styles.container}>
+			<Form action={formAction}>
+				<h1>Add New User</h1>
+				<Input type='text' name='username' placeholder='Enter your username' />
+				<Input type='email' name='email' placeholder='Enter your email' />
+				<Input
+					type='password'
+					name='password'
+					placeholder='Enter your password'
+				/>
+				<Input type='text' name='avatar' placeholder='Add your avatar image' />
+				<Input type='select' name='isAdmin' options={options} />
+				<Button>Add User</Button>
+				{state?.error}
+			</Form>
+		</div>
 	);
 };
--- src/components/formElements/input/Input.jsx
+++ src/components/formElements/input/Input.jsx
@@ -1,18 +1,36 @@
 import styles from './Input.module.css';
 
-export const Input = ({ type, name, placeholder, value }, props) => {
+export const Input = ({ type, name, placeholder, value, options }, props) => {
 	if (type === 'textarea') {
 		return (
 			<textarea
 				rows='5'
 				cols='33'
 				className={styles.inputField}
-				{...props}
 				name={name}
 				placeholder={placeholder}
+				{...props}
 			/>
 		);
 	}
+
+	if (type === 'select' && options) {
+		return (
+			<select
+				className={styles.inputField}
+				name={name}
+				placeholder={placeholder}
+				{...props}
+			>
+				{options.map(({ value, label }) => (
+					<option value={value} key={label}>
+						{label}
+					</option>
+				))}
+			</select>
+		);
+	}
+
 	return (
 		<input
 			className={styles.inputField}
