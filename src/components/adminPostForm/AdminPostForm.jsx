22fdc8839c355c7f9444516e7e3633b663693dd7
Sergii Burgazlieiev <burgazlieiev@gmail.com>
2024-07-07 15:59:54 -0500

    Admin post form refactor

--- src/components/adminPostForm/AdminPostForm.jsx
+++ src/components/adminPostForm/AdminPostForm.jsx
@@ -3,25 +3,25 @@
 import { useFormState } from 'react-dom';
 import { addPost } from '@/lib/actions';
 import styles from './AdminPostForm.module.css';
+import { Form } from '../formElements/form/Form';
+import { Input } from '../formElements/input/Input';
+import { Button } from '../formElements/button/Button';
 
 export const AdminPostForm = ({ session }) => {
 	const [state, formAction] = useFormState(addPost, undefined);
 	const userId = session.user.id;
 	return (
-		<form action={formAction} className={styles.container}>
-			<h1>Add New Post</h1>
-			<input type='hidden' name='userId' value={userId} />
-			<input type='text' name='title' placeholder='Post title' />
-			<input type='text' name='img' placeholder='Post Image' />
-			<input type='text' name='slug' placeholder='Post Slug' />
-			<textarea
-				type='text'
-				name='desc'
-				placeholder='Post Description'
-				rows={10}
-			/>
-			<button>Add Post</button>
-			{state?.error}
-		</form>
+		<div className={styles.container}>
+			<Form action={formAction}>
+				<h1>Add New Post</h1>
+				<Input type='hidden' name='userId' value={userId} />
+				<Input type='text' name='title' placeholder='Post title' />
+				<Input type='text' name='img' placeholder='Post Image' />
+				<Input type='text' name='slug' placeholder='Post Slug' />
+				<Input type='textarea' name='desc' placeholder='Post Description' />
+				<Button>Add Post</Button>
+				{state?.error}
+			</Form>
+		</div>
 	);
 };
